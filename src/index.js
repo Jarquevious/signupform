8b3bca454de7a60be4573f20a4c4942606da8001
Jarquevious <jarquevious@gmail.com>
2021-01-09 11:44:32 -0800

    validation added; test

--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import ReactDOM from "react-dom";
 import { useFormik } from "formik";
 import "./styles.css";
 
+// A custom validation function must return objects which keys are symmetrical to values/initialValues
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    
+    return errors;
+}
+
 const SignupForm = () => {
   const formik = useFormik({
     initialValues: { 
         firstName: "",
         lastName: "",
-        email: "" },
+        email: "" 
+    },
+    validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -25,6 +51,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+       {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
        
        {/* Last Name Input */}
        <label htmlFor="lastName">Last Name</label>
@@ -35,6 +62,8 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
+       {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+
 
        {/* Email Input */}
        <label htmlFor="email">Email Address</label>
@@ -45,6 +74,8 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+       {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
       <button type="submit">Submit</button>
     </form>
   );
