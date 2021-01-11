b1cd10837b7b9b84be94330f8e1fe20b21e7a223
Jarquevious <jarquevious@gmail.com>
2021-01-11 13:42:30 -0800

    input primitive component

--- src/index.js
+++ src/index.js
@@ -16,6 +16,32 @@ const MyTextInput = ({ label, ...props }) => {
   );
 };
 
+const MyCheckbox = ({ children, ...props  }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  return (
+    <div>
+      <label className="checkbox">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? ( 
+      <div className="error">{meta.error}</div>) : null}
+    </div>
+  );
+};
+
+const MySelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props); 
+  return (
+    <div> 
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div> ) : null}
+    </div>
+  );
+};
+
 const SignupForm = () => {
   return (
     <Formik
